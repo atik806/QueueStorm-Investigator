a1dba21e5f6dba928d029eeff4bdc5fa264e3a2e
atik806 <atikrj220@gmail.com>
2026-06-26 21:40:47 +0600

    fix: add error handling to api/index.js, use correct dist paths

--- api/index.js
+++ api/index.js
@@ -1,72 +1,57 @@
 const serverless = require('serverless-http');
 const { NestFactory } = require('@nestjs/core');
-const { ValidationPipe, Logger, BadRequestException, UnprocessableEntityException } = require('@nestjs/common');
+const {
+  ValidationPipe, BadRequestException, UnprocessableEntityException,
+} = require('@nestjs/common');
 const helmet = require('helmet');
 const { AppModule } = require('../dist/app.module');
 const { AllExceptionsFilter } = require('../dist/common/filters/all-exceptions.filter');
 const { LoggingInterceptor } = require('../dist/common/interceptors/logging.interceptor');
 
-let cachedHandler;
+let handler;
 
 async function bootstrap() {
-  if (cachedHandler) return cachedHandler;
+  if (handler) return handler;
 
   const app = await NestFactory.create(AppModule);
 
   app.use(helmet());
   app.enableCors();
 
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: false,
-      transform: true,
-      transformOptions: { enableImplicitConversion: true },
-      exceptionFactory: (errors) => {
-        const messages = errors.map(
-          (e) => `${e.property}: ${Object.values(e.constraints || {}).join(', ')}`,
-        );
-        const message = messages.join('; ');
-
-        const isMissingField = errors.some(
-          (e) => e.value === undefined || e.value === null,
-        );
-
-        const hasOnlySemanticError =
-          !isMissingField &&
-          errors.some((e) => {
-            const constraints = Object.keys(e.constraints || {});
-            return constraints.some(
-              (c) =>
-                c === 'isNotEmpty' ||
-                c === 'isEnum' ||
-                c === 'arrayNotEmpty' ||
-                c === 'minLength' ||
-                c === 'maxLength',
-            );
-          });
-
-        if (hasOnlySemanticError) {
-          return new UnprocessableEntityException(message);
-        }
-
-        return new BadRequestException(message);
-      },
-    }),
-  );
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: false,
+    transform: true,
+    transformOptions: { enableImplicitConversion: true },
+    exceptionFactory: (errors) => {
+      const messages = errors.map((e) => `${e.property}: ${Object.values(e.constraints || {}).join(', ')}`);
+      const message = messages.join('; ');
+      const isMissingField = errors.some((e) => e.value === undefined || e.value === null);
+      const hasOnlySemanticError = !isMissingField && errors.some((e) => {
+        const constraints = Object.keys(e.constraints || {});
+        return constraints.some((c) => c === 'isNotEmpty' || c === 'isEnum' || c === 'arrayNotEmpty' || c === 'minLength' || c === 'maxLength');
+      });
+      if (hasOnlySemanticError) return new UnprocessableEntityException(message);
+      return new BadRequestException(message);
+    },
+  }));
 
   app.useGlobalFilters(new AllExceptionsFilter());
   app.useGlobalInterceptors(new LoggingInterceptor());
 
   await app.init();
 
-  const expressInstance = app.getHttpAdapter().getInstance();
-  cachedHandler = serverless(expressInstance);
+  const expressApp = app.getHttpAdapter().getInstance();
+  handler = serverless(expressApp);
 
-  return cachedHandler;
+  return handler;
 }
 
-module.exports = async function handler(req, res) {
-  const wrappedHandler = await bootstrap();
-  return wrappedHandler(req, res);
+module.exports = async function (req, res) {
+  try {
+    const fn = await bootstrap();
+    return fn(req, res);
+  } catch (err) {
+    res.status(500).json({ error: err.message, stack: err.stack, name: err.name });
+  }
 };
